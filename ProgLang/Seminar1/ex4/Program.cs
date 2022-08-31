0e7a706d962a7c66a1f95829ac4ca2160a7233fa
MariLani <zemlyanikasweet@gmail.com>
2022-09-01 02:23:02 +0300

    Homework_Ex_4

---
+++ ProgLang/Seminar1/ex4/Program.cs
@@ -0,0 +1,17 @@
+﻿int MaxOf3(int number1, int number2, int number3)
+    {
+        int result = number1;
+        if (number2 > result) result = number2;
+        if (number3 > result) result = number3;
+        return(result);
+    }
+
+int GetData()
+    {
+        Console.Write("Enter number ");
+        return(Convert.ToInt32(Console.ReadLine()));
+    }
+
+int max = MaxOf3(GetData(), GetData(), GetData());
+Console.Write("The maximum is: ");
+Console.WriteLine(max);
