c612e36a95878e707d3c478429aaa10542496073
MariLani <zemlyanikasweet@gmail.com>
2022-09-01 02:16:02 +0300

    Homework_Ex_2

---
+++ ProgLang/Seminar1/ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Enter number A>");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number B>");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+    {
+        Console.Write("The bigger is: ");
+        Console.WriteLine(numberA);
+        Console.Write("The lesseer is: ");
+        Console.WriteLine(numberB);
+    }
+else if (numberA == numberB)
+    {
+        Console.WriteLine("Numbers are equal ");
+    }
+else
+    {
+        Console.Write("The bigger is: ");
+        Console.WriteLine(numberB);
+        Console.Write("The lesser is: ");
+        Console.WriteLine(numberA);
+    }
