0740b0e24bf8d32474228a191b8a8ed8fedd1ecd
MariLani <zemlyanikasweet@gmail.com>
2022-09-01 14:11:14 +0300

    Tab

--- ProgLang/Seminar1/ex2/Program.cs
+++ ProgLang/Seminar1/ex2/Program.cs
@@ -5,19 +5,19 @@ Console.Write("Enter number B>");
 int numberB = Convert.ToInt32(Console.ReadLine());
 if (numberA > numberB)
     {
-        Console.Write("The bigger is: ");
-        Console.WriteLine(numberA);
-        Console.Write("The lesseer is: ");
-        Console.WriteLine(numberB);
+    Console.Write("The bigger is: ");
+    Console.WriteLine(numberA);
+    Console.Write("The lesseer is: ");
+    Console.WriteLine(numberB);
     }
 else if (numberA == numberB)
     {
-        Console.WriteLine("Numbers are equal ");
+    Console.WriteLine("Numbers are equal ");
     }
 else
     {
-        Console.Write("The bigger is: ");
-        Console.WriteLine(numberB);
-        Console.Write("The lesser is: ");
-        Console.WriteLine(numberA);
+    Console.Write("The bigger is: ");
+    Console.WriteLine(numberB);
+    Console.Write("The lesser is: ");
+    Console.WriteLine(numberA);
     }
