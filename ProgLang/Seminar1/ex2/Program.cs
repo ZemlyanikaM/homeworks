e8bd26758fdc58988813efa530304adbbbe5a4a4
MariLani <zemlyanikasweet@gmail.com>
2022-09-04 02:30:34 +0300

    Fix the style. Seminar1.

--- ProgLang/Seminar1/ex2/Program.cs
+++ ProgLang/Seminar1/ex2/Program.cs
@@ -4,20 +4,20 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter number B>");
 int numberB = Convert.ToInt32(Console.ReadLine());
 if (numberA > numberB)
-    {
+{
     Console.Write("The bigger is: ");
     Console.WriteLine(numberA);
     Console.Write("The lesseer is: ");
     Console.WriteLine(numberB);
-    }
+}
 else if (numberA == numberB)
-    {
+{
     Console.WriteLine("Numbers are equal ");
-    }
+}
 else
-    {
+{
     Console.Write("The bigger is: ");
     Console.WriteLine(numberB);
     Console.Write("The lesser is: ");
     Console.WriteLine(numberA);
-    }
+}
