ff92ca619bcfb73d0e91a9911fee2cc1b10bd3d3
MariLani <zemlyanikasweet@gmail.com>
2022-09-08 09:20:32 +0300

    Homework_Ex_23

---
+++ ProgLang/Seminar3/ex23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Write("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void Cubes(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine((int)Math.Pow(count, 3));
+        count++;
+    }
+}
+Cubes(N);
