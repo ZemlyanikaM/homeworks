69fad8ac693dcb09028aa25be8d57ece1eef26ef
MariLani <zemlyanikasweet@gmail.com>
2022-09-14 20:52:18 +0300

    Seminar5 Ex38

---
+++ ProgLang/Seminar5/Ex38/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] arr = new double[8];
+FillArray(arr);
+PrintArray(arr);
+double diff = FindMaxMinDiff(arr);
+Console.WriteLine($" maximum - minimum  = {diff}");
+void FillArray(double[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().NextDouble() * 10;
+    }
+}
+void PrintArray(double[] arr)
+{
+    int len = arr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+double FindMaxMinDiff(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    double diff = max - min;
+    Console.WriteLine($"The maximum = {max}");
+    Console.WriteLine($"The minimum = {min}");
+    return diff;
+}
