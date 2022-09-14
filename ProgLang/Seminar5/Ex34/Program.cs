7fe52aa9d3ce3227b5748d2087057ed4430886b8
MariLani <zemlyanikasweet@gmail.com>
2022-09-14 20:08:59 +0300

    Seminar5 Ex34

---
+++ ProgLang/Seminar5/Ex34/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] arr = new int[10];
+FillArray(arr);
+PrintArray(arr);
+int count = FindEven(arr);
+Console.WriteLine($"{count} even numbers in the array");
+void FillArray(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(0, 1000);
+    }
+}
+void PrintArray(int[] arr)
+{
+    int len = arr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+int FindEven(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
