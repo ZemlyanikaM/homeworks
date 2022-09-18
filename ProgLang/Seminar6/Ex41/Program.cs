3184bec3f0b89acdc08d24ca7e3269cef1475116
MariLani <zemlyanikasweet@gmail.com>
2022-09-18 18:03:58 +0300

    Seminar 6. Homework. Ex 41

---
+++ ProgLang/Seminar6/Ex41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+int InputNumber(string msg)
+{
+    Console.Write(msg);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int M = InputNumber("How many numbers do you want to enter? ");
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+    if( InputNumber($"Enter the number {i + 1} > ") > 0) count++;
+}
+Console.WriteLine($"Numbers greater than 0: {count}");
