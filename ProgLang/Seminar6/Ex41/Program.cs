0cb3d3cdedf5d55e961763177830c11ad0455f40
MariLani <zemlyanikasweet@gmail.com>
2022-09-19 10:27:07 +0300

    Fix the style

--- ProgLang/Seminar6/Ex41/Program.cs
+++ ProgLang/Seminar6/Ex41/Program.cs
@@ -11,6 +11,9 @@ int M = InputNumber("How many numbers do you want to enter? ");
 int count = 0;
 for (int i = 0; i < M; i++)
 {
-    if( InputNumber($"Enter the number {i + 1} > ") > 0) count++;
+    if( InputNumber($"Enter the number {i + 1} > ") > 0) 
+    {
+        count++;
+    }
 }
 Console.WriteLine($"Numbers greater than 0: {count}");
